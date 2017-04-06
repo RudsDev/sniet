84794a34d8e97d52aaf1f8bda45d6e6df3638a4c
Rafael Dias <ruds.devel@gmail.com>
2017-04-05 21:07:31 -0300

    Add operação de DELETE ao recurso

--- src/main/java/br/com/resource/api/UsuarioResource.java
+++ src/main/java/br/com/resource/api/UsuarioResource.java
@@ -2,14 +2,17 @@ package br.com.resource.api;
 
 import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
+
 import br.com.app.api.GeraID;
 import br.com.model.api.Usuario;
 import br.com.persist.api.JPAUtil;
@@ -110,5 +113,37 @@ public class UsuarioResource {
 		
 	    return Usuario.userToJson(usuario);
 	}
+
+	@ApiOperation(
+			value="Apaga um usuário do sistema.",
+			consumes = MediaType.TEXT_PLAIN
+		)
+		@ApiResponses(
+			@ApiResponse(
+				code=200,
+				message="Usuário apagado do sistema.",
+				response = Usuario.class
+			)
+		)
+		@Path("{id}")
+		@DELETE
+		@Consumes(MediaType.TEXT_PLAIN)
+		public Response delete(
+				@PathParam(value="id")
+				long id,
+				@Context UriInfo uriInfo){
+			UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+			
+			
+			
+			//Chamada do método e lógica de deleção
+			System.out.println("Usuário deletado com sucesso: " + id); 
+			
+			//TODO Retorna ID do usuário apagado
+			//builder.path(Integer.toString(usuario.getId()));
+
+		    return Response.created(builder.build()).status(200)
+		            .build();
+		}
 	
 }
