7db0a6c209605334d7285e2b5b965949afb49786
Rafael Dias <ruds.devel@gmail.com>
2017-05-07 00:24:12 -0300

    alterando ventre

--- src/main/java/br/com/model/api/Ventre.java
+++ src/main/java/br/com/model/api/Ventre.java
@@ -5,20 +5,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Ventre")
+@Table(name = "Ventre")
 public class Ventre {
-
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
-	@Column(name="codVentre")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_ventre")
+	@SequenceGenerator(name = "seq_ventre", sequenceName = "seq_ventre", initialValue = 1, allocationSize = 1)
+	@Column(name = "CodVentre")
 	private Integer codVentre;
+	
+	@Column(name = "DescCorVentre", length=50)
+	private String descCorVentre;
 
-	@Column(name="descCorVentre")
-	private Integer descCorVentre;
+	public Ventre() {
 
+	}
+
+	@Override
+	public String toString() {
+		return "Ventre [codVentre=" + codVentre + ", descCorVentre=" + descCorVentre + "]";
+	}
 
 	public Integer getCodVentre() {
 		return codVentre;
@@ -28,12 +37,11 @@ public class Ventre {
 		this.codVentre = codVentre;
 	}
 
-	public Integer getDescCorVentre() {
+	public String getDescCorVentre() {
 		return descCorVentre;
 	}
 
-	public void setDescCorVentre(Integer descCorVentre) {
+	public void setDescCorVentre(String descCorVentre) {
 		this.descCorVentre = descCorVentre;
 	}
-
 }
