92387dcba99c553f07687dc5d70eeb0128255d2a
Rafael Dias <ruds.devel@gmail.com>
2017-05-21 11:24:17 -0300

    Criacao EspecieService.

---
+++ src/main/java/br/com/service/api/EspecieService.java
@@ -0,0 +1,29 @@
+package br.com.service.api;
+
+import java.util.List;
+
+import br.com.dao.api.EspecieDao;
+import br.com.model.api.Especie;
+
+public class EspecieService {
+	
+	private EspecieDao dao;
+	
+	
+	public EspecieService(){
+		this.dao = new EspecieDao();
+	}
+	
+	public  Especie searchByID(String id){
+		return this.dao.getEspecieById(id);
+	}
+	
+	public  Especie searchByNomeCientifico(String nomeCientifico){
+		return this.dao.getEspecieByNomeCientifico(nomeCientifico);
+	}
+	
+	public List<Especie> getAllEspecies(){
+		return this.dao.buscarTodasAsEspecies();
+	}
+	
+}
