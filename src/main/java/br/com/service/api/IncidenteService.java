b81272325368ac3f4f68449d79da5ba57b2ec863
Rafael Dias <ruds.devel@gmail.com>
2017-05-21 19:07:13 -0300

    Criacao IncidenteService.

---
+++ src/main/java/br/com/service/api/IncidenteService.java
@@ -0,0 +1,30 @@
+package br.com.service.api;
+
+import java.util.Date;
+import java.util.List;
+import br.com.dao.api.IncidenteDao;
+import br.com.model.api.Incidente;
+
+
+public class IncidenteService {
+
+	private IncidenteDao dao;
+	
+	
+	public IncidenteService(){
+		this.dao = new IncidenteDao();
+	}
+	
+	public  Incidente searchByID(Integer id){
+		return this.dao.getIncidenteById(id);
+	}
+	
+	public  List<Incidente> searchByPeriodo(Date dataInicial, Date dataFinal){
+		return this.dao.getByPeriodo(dataInicial, dataFinal);
+	}
+	
+	public List<Incidente> getAllIncidentes(){
+		return this.dao.buscarTodosInicidentes();
+	}
+	
+}
