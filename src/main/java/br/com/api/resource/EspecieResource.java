cc4db45c473b2deaa3c3940b17426229809ce49d
Rafael Dias <ruds.devel@gmail.com>
2017-08-21 22:03:14 -0300

    Add método salvar em EspecieResource.

--- src/main/java/br/com/api/resource/EspecieResource.java
+++ src/main/java/br/com/api/resource/EspecieResource.java
@@ -4,31 +4,89 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
-
 import br.com.api.model.Especie;
 import br.com.api.service.EspecieService;
 import br.com.api.util.Util;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ResponseHeader;
 
 @Api
 @Path("/especies")
 public class EspecieResource {
 	
-	private EspecieService service;
+	private EspecieService especieService;
 
 	public EspecieResource() {
-		this.service = new EspecieService();
+		this.especieService = new EspecieService();
 	}
+	
+	@ApiOperation(
+			value="Salva um especie no sistema.",
+			consumes = MediaType.APPLICATION_JSON,
+			produces = MediaType.APPLICATION_JSON
+		)
+		@ApiResponses(
+			@ApiResponse(
+				code=201,
+				message="especie salva no sistema.",
+				response = Especie.class,
+				responseHeaders=
+					@ResponseHeader(
+						name="Location",
+						description="uri especie criada.",
+						response=String.class
+					)
+			)
+		)
+		@Path("/")
+		@POST
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response save(
+				@ApiParam(
+					value="especie",
+					name="especieJson",
+					required=true
+				)
+				String especieJson,
+				@Context UriInfo uriInfo, @Context HttpHeaders headers){
+			
+			/*Integer acessLevel = 1;
+			
+			String token = headers.getHeaderString("Authorization");
+			
+			try {
+				//MyTokenGen.verifyTokenAcess(token, acessLevel);*/
+				
+				UriBuilder builder = uriInfo.getAbsolutePathBuilder();		
+
+				Especie especieSaved = this.especieService.save((Especie) 
+						Util.jsonToObject(especieJson, Especie.class));
+				
+				//Coloca o ID do user recém salvo na resposta para o client (Location)
+				builder.path(Integer.toString(especieSaved.getIdEspecie()));
+				
+			    return Response.created(builder.build()).status(201)
+			            .build();
+			/*}
+			catch (UnauthorizedAcessException e) {
+				e.printStackTrace();
+			    return Response.status(401).build();
+			}*/
+		}
 	
 	
 	@ApiOperation(
@@ -48,7 +106,7 @@ public class EspecieResource {
 	public Response buscaTodasEspecies(
 			@Context UriInfo uriInfo){
 		
-		List<Especie> especies = this.service.getAllEspecies();
+		List<Especie> especies = this.especieService.getAllEspecies();
 		List<String> especiesJson = new ArrayList<>();
 		
 		for(int i=0; i<especies.size();i++){
@@ -80,7 +138,7 @@ public class EspecieResource {
 			int id,
 			@Context UriInfo uriInfo){
 		
-		Especie especie = this.service.searchByID(id);
+		Especie especie = this.especieService.searchByID(id);
 		
 		String especieJson = Util.objectToJson(especie);
 
@@ -109,7 +167,7 @@ public class EspecieResource {
 			String nomeCientifico,
 			@Context UriInfo uriInfo){
 		
-		Especie especie = this.service.searchByNomeCientifico(nomeCientifico);
+		Especie especie = this.especieService.searchByNomeCientifico(nomeCientifico);
 		String especieJson = Util.objectToJson(especie);
 
 		return Response.ok(especieJson.toString(), MediaType.APPLICATION_JSON).build();
